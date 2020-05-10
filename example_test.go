5afa61beaf9adfa74d301bfa61773bff16bf6f7c
Foad Nosrati Habibi <foad.habibi@shopee.com>
2020-05-10 15:46:30 +0800

    Add example

---
+++ example_test.go
@@ -0,0 +1,47 @@
+package healthcheck
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+)
+
+func Example() {
+	ctx := context.Background()
+	var serviceIsUnhealthy bool
+	dummyHealthyChecker := func(_ context.Context) error {
+		return nil
+	}
+	dummyUnhealthyChecker := func(_ context.Context) error {
+		if serviceIsUnhealthy {
+			return errors.New("not_feeling_good")
+		} else {
+			return nil
+		}
+	}
+
+	h := New()
+	h.Register("dummy_healthy_checker", dummyHealthyChecker, time.Second)
+	h.Register("dummy_unhealthy_checker_in_background", dummyUnhealthyChecker, time.Second*2, InBackground(time.Millisecond))
+	h.Register("dummy_unhealthy_checker_with_threshold", dummyUnhealthyChecker, time.Second, WithThreshold(2))
+	h.Run(ctx)
+	defer h.Close()
+
+	time.Sleep(time.Millisecond * 10)
+
+	// check method is not exposed. Don't use it in your codes.
+	fmt.Println(h.check(ctx))
+
+	// Lets make unhealthy checkers fail
+	serviceIsUnhealthy = true
+	time.Sleep(time.Millisecond * 10)
+
+	fmt.Println(h.check(ctx))
+	fmt.Println(h.check(ctx))
+
+	// Output:
+	// map[]
+	// map[dummy_unhealthy_checker_in_background:not_feeling_good]
+	// map[dummy_unhealthy_checker_in_background:not_feeling_good dummy_unhealthy_checker_with_threshold:not_feeling_good]
+}
